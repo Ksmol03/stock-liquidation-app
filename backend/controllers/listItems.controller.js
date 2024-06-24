d833bf6256255529cec182944cd686397b5abfd7
Kacper <kacper.smolinski@hotmail.com>
2024-06-24 10:01:37 +0200

    feat: send items data to user

--- backend/controllers/listItems.controller.js
+++ backend/controllers/listItems.controller.js
@@ -1,14 +1,18 @@
+import { queryDatabase } from "../database/mySQL.database.js";
 import { findPermissionUtil } from "../utils/findPermission.util.js";
 
 export const listItemsController = async (req, res) => {
     try {
+        //Check user's permission
         const canUserReadItems = await findPermissionUtil('canReadItems', res.locals.username);
-        
         if (!canUserReadItems) {
             return res.status(401).json({message: 'Unauthorized.'});
         }
 
-        return res.json({message: 'git'});
+        const listItemsQuery = 'SELECT * FROM items';
+        const itemsList = await queryDatabase(listItemsQuery);
+
+        return res.json(itemsList);
 
     }   catch (error) {
         console.error(error);
