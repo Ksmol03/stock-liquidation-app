4eb7bb0ba9e90f5bd282aa204fe016cedc1d258c
Kacper <kacper.smolinski@hotmail.com>
2024-06-24 08:23:11 +0200

    feat: add authentication middleware

---
+++ backend/middleware/authenticateUser.middleware.js
@@ -0,0 +1,27 @@
+import { queryDatabase } from "../database/mySQL.database.js";
+
+export const authenticateUserController = async (req, res, next) => {
+    const sessionKey = req.cookies.sessionKey;
+
+    //Check if user is logged in
+    if (!sessionKey) {
+        return res.status(401).json({message: 'Unauthenticated.'});
+    }
+
+    try {
+        //Find username based on session key
+        const findUsernameQuery = 'SELECT username FROM sessions WHERE session_key = ?';
+        const foundUsername = await queryDatabase(findUsernameQuery, [sessionKey]);
+
+        if (foundUsername.length == 0) {
+            return res.status(401).json({message: 'Unauthenticated.'});
+        }
+
+        //Save found username in res.locals
+        res.locals.username = foundUsername[0].username;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error.'});
+    }
+}
--- backend/routes/api.router.js
+++ backend/routes/api.router.js
@@ -2,8 +2,12 @@ import express from 'express';
 import { queryDatabase } from '../database/mySQL.database.js';
 import { logInController } from '../controllers/logIn.controller.js';
 import { logOutController } from '../controllers/logOut.controller.js';
+import { authenticateUserController } from '../middleware/authenticateUser.middleware.js';
 
 export const router = express.Router();
 
 router.get('/logIn', logInController);
 router.get('/logOut', logOutController);
+router.get('/auth', authenticateUserController, (req, res) => {
+    return res.json({message: `Hello ${res.locals.username}!`});
+});
