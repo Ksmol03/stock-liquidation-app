8599c305d979b58b7f00025db76b0c5a74fb39f8
Kacper <kacper.smolinski@hotmail.com>
2024-06-25 19:29:47 +0200

    fead: update items

---
+++ backend/controllers/updateItem.controller.js
@@ -0,0 +1,34 @@
+import { queryDatabase } from "../database/mySQL.database.js";
+import { findPermissionUtil } from "../utils/findPermission.util.js"
+
+export const updateItemController = async (req, res) => {
+    const {liquidationDate, itemId} = req.body;
+
+    //Check if arguments are passed
+    if (!liquidationDate || !itemId) {
+        return res.status(400).json({message: 'Bad request.'})
+    }
+
+    try {
+        //Check user's permission
+        const canUserUpdateItems = await findPermissionUtil('canUpdateItems' ,res.locals.username);
+        if(!canUserUpdateItems) {
+            return res.status(401).json({message: 'Unauthorized.'});
+        }
+
+        //Update item
+        const updateItemQuery = 'UPDATE items SET liquidation_date = ? WHERE item_id = ? AND liquidation_date IS NULL';
+        const updateItemResult = await queryDatabase(updateItemQuery, [liquidationDate, itemId]);
+
+        //Check if item was updated
+        if (updateItemResult.affectedRows == 0) {
+            return res.status(400).json({message: 'Bad request.'});
+        }
+
+        return res.json({message: 'Updated successfully.'});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal server error.'});
+    }
+}
--- backend/routes/api.router.js
+++ backend/routes/api.router.js
@@ -3,6 +3,7 @@ import { logInController } from '../controllers/logIn.controller.js';
 import { logOutController } from '../controllers/logOut.controller.js';
 import { authenticateUserMiddleware } from '../middleware/authenticateUser.middleware.js';
 import { listItemsController } from '../controllers/listItems.controller.js';
+import { updateItemController } from '../controllers/updateItem.controller.js';
 
 export const router = express.Router();
 
@@ -10,4 +11,5 @@ router.get('/logIn', logInController);
 router.get('/logOut', logOutController);
 
 router.use(authenticateUserMiddleware);
-router.get('/listItems', listItemsController)
+router.get('/listItems', listItemsController);
+router.get('/updateItem', updateItemController);
