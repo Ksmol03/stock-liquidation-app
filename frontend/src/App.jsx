f66fe4c90ddb40e15bef9609d54a1014504480b5
Kacper <kacper.smolinski@hotmail.com>
2024-06-27 19:44:03 +0200

    feat: frontend login setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
+  const authenticateUser = () => {
+    axios.get('/api/authenticate')
+      .then(response => {
+        if (response.status == 200) {
+          console.log('Authenticated!');
+          setUserData(response.data);
+        }
+      })
+      .catch(err => console.log('Error: ', err));
+  }
+
+  const logIn = () => {
+    axios.post('/api/login', {username: 'admin', password: 'admin'})
+      .then(() => {
+        authenticateUser();
+      });
+  };
+
+  const logOut = () => {
+    axios.delete('/api/logout');
+    setUserData(null);
+  }
+
+  if (userData) {
+    return (
+      <>
+        <div>Hello, {userData.username}!</div>
+        <button onClick={logOut}>Log Out</button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div>Please, Log In</div>
+        <button onClick={logIn}>Log In</button>
+      </>
+    )
+  }
 }
 
 export default App
